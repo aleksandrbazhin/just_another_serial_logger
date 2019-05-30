2c92992796fa0069a8fa384832170182c2d6d41e
Aleksandr Bazhin <aleksandrbahzin@yandex.ru>
2019-05-30 13:07:01 +1000

    first version works

--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,11 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QSerialPort>
+#include <QMessageBox>
+#include <QString>
+#include <QStringList>
+#include <QDateTime>
 
 namespace Ui {
 class MainWindow;
@@ -16,7 +21,19 @@ public:
     ~MainWindow();
 
 private:
+    void handleConnectButton();
+    bool openSerialPort();
+    void closeSerialPort();
+    void readData();
+    void startRecording();
     Ui::MainWindow *ui;
+    QSerialPort *serial = nullptr;
+    bool serial_connected = false;
+    bool recording_started = false;
+    qint64 recording_start_time = 0;
+    bool data_header_received = false;
+    QString data_to_save = "";
+
 };
 
 #endif // MAINWINDOW_H
